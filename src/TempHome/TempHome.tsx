0c0f50963d3187b0766d33308fbb96f69870038e
Chris Mochinski <cmochinski@gmail.com>
2024-05-16 21:41:50 -0500

    ridiculous logo work DONE...will probably delete later

---
+++ src/TempHome/TempHome.tsx
@@ -0,0 +1,19 @@
+import "./TempHome.scss";
+
+export function TempHome() {
+  return (
+    <div className="tempHome centerText">
+      <div className="logoText">
+        <h1>___</h1>
+        <h1>The</h1>
+        <h1>___</h1>
+      </div>
+      <div className="logoText">
+        <h1>_____</h1>
+        <h1>Brook</h1>
+        <h1>_____</h1>
+      </div>
+      <h2 className="subLogoText">Tattoo Studio</h2>
+    </div>
+  );
+}
