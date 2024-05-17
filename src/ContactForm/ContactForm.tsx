7fb168bda2212f4ba4fd6ae1af9ad8fd06c1b8a5
Chris Mochinski <cmochinski@gmail.com>
2024-05-17 00:16:36 -0500

    before form work

--- src/ContactForm/ContactForm.tsx
+++ src/ContactForm/ContactForm.tsx
@@ -2,17 +2,16 @@ import { useState } from "react";
 import "./ContactForm.scss";
 
 export function ContactForm() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [concept, setConcept] = useState("");
-    const [artist, setArtist] = useState("");  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [concept, setConcept] = useState("");
+  const [artist, setArtist] = useState("");
 
   const handleSubmit = (event: any) => {
-    event.preventDefault();  
-    const form = event.target.form;  
-    form.submit();  
-
+    event.preventDefault();
+    const form = event.target.form;
+    form.submit();
   };
 
   return (
@@ -40,27 +39,29 @@ export function ContactForm() {
 
         <textarea id="concept" name="concept" maxLength={500} required></textarea>
 
-      
-        <select id="artist" name="artist">
+        <select id="artist" name="artist" defaultValue="ARTIST">
           <option value="Artist" disabled selected>
             ARTIST
           </option>
           <option value="jamesermilio01@gmail.com">Jimbo</option>
           <option value="tattoos.by.cr@gmail.com">Carlie</option>
+          {/* if user selects "Not Sure", value should be BOTH emails somehow */}
           <option value="tattoos.by.cr@gmail.com">Not Sure</option>
         </select>
 
-        <div className="submitButton"
-             tabIndex={0}
-             role="button"
-             onClick={handleSubmit}
-             onKeyPress={(event) => {
-               if (event.key === 'Enter' || event.key === ' ') {
-                 handleSubmit(event);
-               }
-             }}
-        >
-          Send
+        <div
+          className="submitButtonContainer"
+          tabIndex={0}
+          role="button"
+          onClick={handleSubmit}
+          onKeyPress={(event) => {
+            if (event.key === "Enter" || event.key === " ") {
+              handleSubmit(event);
+            }
+          }}>
+          <div className="submitButton">
+            <h4>Send</h4>
+          </div>
         </div>
       </form>
     </div>
