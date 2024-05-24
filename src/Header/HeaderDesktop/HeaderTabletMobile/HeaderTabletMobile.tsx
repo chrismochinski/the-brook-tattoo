698247ca39e589ca99b756800ae416e78fbbecb1
Chris Mochinski <cmochinski@gmail.com>
2024-05-24 17:44:04 -0500

    idea - shadow combo - need to tighten up

--- src/Header/HeaderDesktop/HeaderTabletMobile/HeaderTabletMobile.tsx
+++ src/Header/HeaderDesktop/HeaderTabletMobile/HeaderTabletMobile.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "./HeaderTabletMobile.scss";
 import Logo from "../../../Images/brook-logo-nobg-rough.png";
 
+// idea https://codepen.io/bugrakocak/pen/xodpBR
+
 export function HeaderTabletMobile() {
   const [isOpen, setIsOpen] = useState(false);
 
