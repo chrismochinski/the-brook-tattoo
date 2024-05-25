ba444d85bbc90d98ac4d447362b7e16808c85df2
Chris Mochinski <cmochinski@gmail.com>
2024-05-25 02:07:58 -0500

    homepage image center styles stuff

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,14 +3,25 @@ import "./Home.scss";
 
 export function Home() {
   return (
-    <div className="home row">
-      <div className="imageWrapper">
-        <img
-          width="100%"
-          className="storeFrontImage"
-          src={StoreFront}
-          alt="The Brook Tattoo Studio Header Logo"
-        />
+    <div className="home">
+      <div className="imageWrapper section">
+        <div className="imageInnerWrapper">
+          <img
+            width="100%"
+            className="storeFrontImage"
+            src={StoreFront}
+            alt="The Brook Tattoo Studio Header Logo"
+          />
+          <div className="titleOverlay">
+            <div className="titleBar">
+              <h1 className="backgroundTitle">The Brook Tattoo</h1>
+              <h1>The Brook Tattoo</h1>
+            </div>
+            <div className="subtitleBar">
+              <h2>Hastings, Minnesota</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
